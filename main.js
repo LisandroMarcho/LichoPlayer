101c4e579944e8f2b01e4c224fdc6cd582b2d4db
LisandroMarcho <djlm2140@gmail.com>
2021-05-11 19:49:08 -0300

    Añadida barra de progreso funcional

--- main.js
+++ main.js
@@ -3,35 +3,41 @@ const playList = ["song1", "song2", "song3", "song4", "song5"];
 var LichoPlayer = null;
 var playButton = null;
 var stopButton = null;
+var progressBar = null;
 
-window.onload = () => {
-  // Grabbing from DOM and events
-  LichoPlayer = document.getElementById("audio");
-  LichoPlayer.onended = stopSong;
+window.onload = async function () {
+  LichoPlayer   = document.getElementById("audio");
+  playButton    = document.getElementById("play");
+  stopButton    = document.getElementById("stop");
+  progressBar   = document.getElementById("progress");
+  timeLabel     = document.getElementById("currentTime");
+  durationLabel = document.getElementById("duration");
 
-  playButton = document.getElementById("play");
-  playButton.onclick = playPause;
+  LichoPlayer.onended = songEnded;
+  LichoPlayer.ontimeupdate = updateProgressBar
+  LichoPlayer.onloadedmetadata = metaLoaded; 
 
-  stopButton = document.getElementById("stop");
-  stopButton.onclick = stopSong;
+  progressBar.onchange = clickOnProgressBar;
 
-  loadSong(playList[0]);
+  await loadSong(playList[0]);
 };
 
-function loadSong(song) {
+async function loadSong(song) {
   stopSong();
 
   try {
     LichoPlayer.src = `songs/${song}.mp3`;
-    LichoPlayer.load();
+    await LichoPlayer.load();
     LichoPlayer.dataset.state = "loaded";
     LichoPlayer.dataset.file = song;
   } catch (error) {
     console.error(error);
   }
+
+  progressBar.value = 0;
 }
 
-function playPause() {
+async function playPause() {
   switch (LichoPlayer.dataset.state) {
     case "playing":
       LichoPlayer.pause();
@@ -42,6 +48,7 @@ function playPause() {
       LichoPlayer.play();
       LichoPlayer.dataset.state = "playing";
       playButton.innerText = "Pause";
+      break;
   }
 
   stopButton.disabled = false;
@@ -54,34 +61,66 @@ function stopSong() {
 
   playButton.innerText = "Play";
   stopButton.disabled = true;
+
+  progressBar.value = 0;
 }
 
-function nextSong() {
+async function nextSong() {
   let songIndex = playList.indexOf(LichoPlayer.dataset.file);
   if (songIndex != playList.length - 1) {
-    loadSong(playList[songIndex + 1]);
+    await loadSong(playList[songIndex + 1]);
     playPause();
   }
   else {
-    loadSong(playList[0]);
+    await loadSong(playList[0]);
     playPause();
   }
 }
 
-function previusSong() {
+async function previusSong() {
   let songIndex = playList.indexOf(LichoPlayer.dataset.file);
   if (songIndex != 0) {
-    loadSong(playList[songIndex - 1]);
+    await loadSong(playList[songIndex - 1]);
     playPause();
   }
   else {
-    loadSong(playList[playList.length - 1]);
+    await loadSong(playList[playList.length - 1]);
     playPause();
   }
 }
 
-function selectFromList (song) {
+async function playFromList (song) {
   let songIndex = playList.indexOf(song.dataset.file);
-  loadSong(playList[songIndex]);
+  await loadSong(playList[songIndex]);
   playPause();
+}
+
+function songEnded () {
+  let songIndex = playList.indexOf(LichoPlayer.dataset.file);
+  if((songIndex + 1) == playList.length) stopSong();
+  else nextSong();
+}
+
+function updateProgressBar () {
+  let currentTime = Math.floor(LichoPlayer.currentTime);
+  let minutes = parseInt(currentTime / 60);
+  let seconds = currentTime - minutes * 60;
+  
+  if(seconds < 10) seconds = `0${currentTime - minutes * 60}`;
+
+  timeLabel.innerText = `${minutes}:${seconds}`;
+  progressBar.value = currentTime;
+}
+
+function metaLoaded () {
+  let duration = Math.floor(LichoPlayer.duration);
+  let minutes = parseInt(duration / 60);
+  let seconds = duration - minutes * 60;
+  
+  durationLabel.innerText = `${minutes}:${seconds}`;
+  progressBar.max = Math.floor(LichoPlayer.duration);
+}
+
+function clickOnProgressBar () {
+  LichoPlayer.currentTime = progressBar.value;
 }
