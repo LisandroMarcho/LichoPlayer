4212acdd097f281eeaa3a0abf29549dc5ff4fb98
LisandroMarcho <djlm2140@gmail.com>
2021-05-11 17:54:04 -0300

    Función de canción siguiente y anterior terminadas

--- main.js
+++ main.js
@@ -1,61 +1,87 @@
-const songs = ["song1", "song2", "song3", "song4", "song5"];
+const playList = ["song1", "song2", "song3", "song4", "song5"];
 
 var LichoPlayer = null;
 var playButton = null;
 var stopButton = null;
 
 window.onload = () => {
-    // Grabbing from DOM and events
-    LichoPlayer = document.getElementById("audio");
-    LichoPlayer.onended = songEnded;
+  // Grabbing from DOM and events
+  LichoPlayer = document.getElementById("audio");
+  LichoPlayer.onended = stopSong;
 
-    playButton = document.getElementById("play");
-    playButton.onclick = playPause;
-    
-    stopButton = document.getElementById("stop");
-    stopButton.onclick = stop;
+  playButton = document.getElementById("play");
+  playButton.onclick = playPause;
 
-    try {
-        LichoPlayer.src = `songs/${songs[0]}.mp3`;
-        loadSong();
-    } catch (e) {
-        console.error(e);
-    }
-    
+  stopButton = document.getElementById("stop");
+  stopButton.onclick = stopSong;
 
-    console.log(LichoPlayer);
+  loadSong(playList[0]);
 };
 
-async function loadSong() {
-    await LichoPlayer.load();
+function loadSong(song) {
+  stopSong();
+
+  try {
+    LichoPlayer.src = `songs/${song}.mp3`;
+    LichoPlayer.load();
     LichoPlayer.dataset.state = "loaded";
+    LichoPlayer.dataset.file = song;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function songEnded () {
-    alert("song ended");
-}
-
-function playPause () {
-    switch (LichoPlayer.dataset.state) {
+function playPause() {
+  switch (LichoPlayer.dataset.state) {
     case "playing":
-        LichoPlayer.pause();
-        LichoPlayer.dataset.state = "paused";
-        playButton.innerText = "Play";
-        break;
+      LichoPlayer.pause();
+      LichoPlayer.dataset.state = "paused";
+      playButton.innerText = "Play";
+      break;
     default:
-        LichoPlayer.play();
-        LichoPlayer.dataset.state = "playing";
-        playButton.innerText = "Pause";
-    }
+      LichoPlayer.play();
+      LichoPlayer.dataset.state = "playing";
+      playButton.innerText = "Pause";
+  }
 
-    stopButton.disabled = false;
+  stopButton.disabled = false;
 }
 
-function stop () {
-    LichoPlayer.pause();
-    LichoPlayer.currentTime = 0;
-    LichoPlayer.dataset.state = "stoped";
-    
-    playButton.innerText = "Play";
-    stopButton.disabled = true;
+function stopSong() {
+  LichoPlayer.pause();
+  LichoPlayer.currentTime = 0;
+  LichoPlayer.dataset.state = "stoped";
+
+  playButton.innerText = "Play";
+  stopButton.disabled = true;
+}
+
+function nextSong() {
+  let songIndex = playList.indexOf(LichoPlayer.dataset.file);
+  if (songIndex != playList.length - 1) {
+    loadSong(playList[songIndex + 1]);
+    playPause();
+  }
+  else {
+    loadSong(playList[0]);
+    playPause();
+  }
+}
+
+function previusSong() {
+  let songIndex = playList.indexOf(LichoPlayer.dataset.file);
+  if (songIndex != 0) {
+    loadSong(playList[songIndex - 1]);
+    playPause();
+  }
+  else {
+    loadSong(playList[playList.length - 1]);
+    playPause();
+  }
+}
+
+function selectFromList (song) {
+  let songIndex = playList.indexOf(song.dataset.file);
+  loadSong(playList[songIndex]);
+  playPause();
 }
